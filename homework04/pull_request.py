f76dc43bfe7fde13fc84144dba8123e8c3969ec5
vacilio <admin@ks.rniirs.ru>
2021-09-14 13:36:56 +0300

    add user with labels

--- homework04/pull_request.py
+++ homework04/pull_request.py
@@ -2,6 +2,15 @@ import requests
 import sys
 
 
+def display_count_user_labels(label_dict):
+    print('\n\n***************************************************************')
+    print('\tUsers with labels:')
+    print('***************************************************************')
+    for user in label_dict:
+        if (label_dict[user] > 0):
+            print(f"\t{user} - {label_dict[user]} labels")
+
+
 def display_active_user(sorted_user):
     if len(sorted_user) == 0:
         return
@@ -11,11 +20,9 @@ def display_active_user(sorted_user):
     for user in sorted_user:
         if user[1] > 1:
             print(f"\t{user[0]} - ", user[1], ' pull requests')
-            # print(f"\t{user[0]} - \t{user[1]} pull requests")
 
 
 if __name__ == '__main__':
-    # url = 'https://github.com/t1m0n/air-datepicker'
     if len(sys.argv) > 1:
         url = sys.argv[1]
     else:
@@ -27,6 +34,7 @@ if __name__ == '__main__':
     page = 1
     open_request = 0
     users = []
+    label_dict = {}
     while True:
         r = requests.get(
             f'https://api.github.com/repos/{git_user}/{git_repo}/pulls?per_page=100&page={page}&state=open')
@@ -38,10 +46,16 @@ if __name__ == '__main__':
         for elem in result:
             user = elem['user']['login']
             users.append(user)
+            labels_count = len(elem['labels'])
+            if user in label_dict:
+                label_dict[user] = label_dict[user] + labels_count
+            else:
+                label_dict[user] = labels_count
         page = page + 1
 
     users_dict = dict((i, users.count(i)) for i in users)
     sorted_user = [(k, users_dict[k]) for k in sorted(
         users_dict, key=users_dict.get, reverse=True)]
     display_active_user(sorted_user)
+    display_count_user_labels(label_dict)
     print(f'\n\nIn repo {url} open request: {open_request}\n\n')
